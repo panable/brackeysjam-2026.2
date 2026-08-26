7b14bd17d21e640ab782d3b5a10d07c12526b682
Dhanveer Ramnauth <dhanr1606@gmail.com>
2026-08-27 02:35:42 +1000

    fix dungeon 13, and make dungeon 9 load as placeholder north

--- Scenes/world_manager.gd
+++ Scenes/world_manager.gd
@@ -10,11 +10,13 @@ func transition_dungeon(destination_room: String, destination_entrace: String):
 	playa.set_collision_layer_value(1, false)
 	playa.set_physics_process(false)
 	
+
+	
 	current_dungeon.queue_free()
 	await get_tree().physics_frame
 	var dungeon_scene := load(destination_room) as PackedScene
 	current_dungeon = dungeon_scene.instantiate() as Dungeon
-
+	print("current_dungeon is " + current_dungeon.name)
 	get_tree().current_scene.add_child(current_dungeon)
 	
 	var exit: Marker3D = current_dungeon.get_node_or_null("Doorways/Exits/" + destination_entrace)
