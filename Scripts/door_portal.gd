30fa2d9023520c480950cb8a203380b1438e4008
Dhanveer Ramnauth <dhanr1606@gmail.com>
2026-08-27 08:40:55 +1000

    add rotation ability

--- Scripts/door_portal.gd
+++ Scripts/door_portal.gd
@@ -1,6 +1,8 @@
 class_name DoorPortal
 extends Area3D
 @onready var dungeon: Dungeon = $"../../.."
+@export var rotation_amt: float = 0
+
 signal player_entered(destination_room: String, destination_entrace: String)
 
 @export_file("Scenes/Dungeons/*.tcsn")
@@ -13,4 +15,6 @@ func _ready() -> void:
 	player_entered.connect(dungeon._on_door_portal_player_entered)
 
 func _on_body_entered(body: Node3D) -> void:
+	var player = body as Player
+	player.pivot.rotate_y(deg_to_rad(rotation_amt))
 	player_entered.emit(destination_room, destination_entrace)
