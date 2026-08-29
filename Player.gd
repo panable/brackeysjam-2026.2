7fff4828a31f4795d5fe3963100281b996abb0b4
Dhanveer Ramnauth <dhanr1606@gmail.com>
2026-08-30 03:57:46 +1000

    fix corrupt playas

--- Player.gd
+++ Player.gd
@@ -17,7 +17,7 @@ func _process(_delta):
 	var mouse := get_viewport().get_mouse_position()
 	var center := get_viewport().get_visible_rect().size / 2.0
 
-	var direction := center - mouse
+	var direction := mouse - center
 
 	pivot.rotation.y = atan2(direction.x, direction.y)
 
